2ac267791d54c07c1d4b9d2a9f33c27b6f5c201f
Jessica <cf17jessica.hernandez@iesjoandaustria.org>
2017-12-20 16:29:01 +0100

    Añadir archivo

---
+++ M5UF2Ex2/Juego/textos.java
@@ -0,0 +1,24 @@
+
+
+
+public class textos {
+	
+	public static void main (String[] args) {
+		
+		System.out.println("\uD83C\uDCBF"+" TE DAMOS LA BIENVENIDA AL BLACK JACK "+"\uD83C\uDCBF");
+		System.out.println("---------------------------------------------");
+		System.out.println("Aqui tiene el paquete de bienvenida: 100฿"+"\nApueste con moderacion, es su responsabilidad");
+		System.out.println("");
+		System.out.print("¿Cuanto quieres apostar? ");
+		 
+		  
+		
+		
+		
+		
+			 
+		  
+		
+	}
+}
+
